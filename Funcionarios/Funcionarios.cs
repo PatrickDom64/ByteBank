ded31275b1790f306d8b220b849d77a98af6bce1
Patrick Dom <patrickdomsilva@gmail.com>
2024-03-28 01:16:30 -0300

    Adicionar arquivos de projeto.

---
+++ Funcionarios/Funcionarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    internal class Funcionario
+    {
+        //0- Fincionario 0,1
+        //1- Diretor 1
+        private int _tipo;
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario * 2;
+            else
+                return Salario * 0.10;
+        }
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank{
+    class Program{
+        static void Main(string[] args) {
+            
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao(); 
+            
+            //Funcionario
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.975.157-10";
+            carlos.Salario = 2400;
+
+            Console.WriteLine(carlos.GetBonificacao());
+            Console.ReadLine();
+
+            //Diretora
+            Funcionario roberta = new Funcionario(2);
+
+            roberta.Nome = "Roberta";
+            roberta.CPF = "548.645.852-90";
+            roberta.Salario = 9000;
+
+            Console.WriteLine(roberta.GetBonificacao());
+            Console.ReadLine();
+
+        }
+    }
+}
